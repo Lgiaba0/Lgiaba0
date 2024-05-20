ac8413bb0bf992f05cb52cc36aa66f621ffa6808
Lgiaba0 <lgiabao152@gmail.com>
2024-05-20 09:37:47 +0700

    Create VL16.cpp

---
+++ VL16.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ucln (int a, int b) {
+    for (int i = max (abs(a),abs(b)); i >= min (abs(a),abs(b)); i--) {
+        if (a % i == 0 && b % i == 0) {
+            return i;
+        }
+    }
+}
+int main () {
+    int a,b;
+    cin >> a >> b;
+    cout << abs((a*b))/ucln (a,b);
+}
