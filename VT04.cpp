c0eb8d7f7489f954511d6d0c8961120e71082a89
Lgiaba0 <lgiabao152@gmail.com>
2024-05-27 19:11:29 +0700

    Create VT04.cpp

---
+++ VT04.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check (long long a[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            return true;
+        }
+    }
+    return false;
+} 
+int main () {
+    int n, x;
+    cin >> n >> x;
+    long long a[n]; 
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << ( check(a,n,x) ? "YES" : "NO")
+}
