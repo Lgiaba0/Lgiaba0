43d9cb22c406ccb4c0f08cb2c48644ca4c68cf61
Lgiaba0 <lgiabao152@gmail.com>
2025-03-17 08:27:40 +0700

    Create if_else2.c

---
+++ if_else2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main () {
+	int a;
+	scanf ("%d", &a);
+	if ( a == 100) {
+		printf ("%d bang 100", a);
+	}
+	else if ( a > 100 ) {
+		printf ("%d lon hon 100", a);
+	}
+	else printf ("%d be hon 100", a);
+}
