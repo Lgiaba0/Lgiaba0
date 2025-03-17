959c9311a7a84fda16e7408e6275408803a56e68
Lgiaba0 <lgiabao152@gmail.com>
2025-03-17 09:47:37 +0700

    Create if_else5.c

---
+++ if_else5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main () {
+    int a;
+    scanf ("%d", &a);
+    if ( a == 0) printf ("%d la so 0", a);
+    else if ( a > 0) printf ("%d la so nguyen duong", a);
+    else printf ("%d la so nguyen am", a); 
+}
